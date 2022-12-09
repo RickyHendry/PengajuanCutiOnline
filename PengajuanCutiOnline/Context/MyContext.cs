a17e8a724acf31332867bcad4805a068cc48d859
Hendri <rickyhendry05@gmail.com>
2022-12-09 23:08:33 +0800

    Update
    9/12/2022

--- PengajuanCutiOnline/Context/MyContext.cs
+++ PengajuanCutiOnline/Context/MyContext.cs
@@ -1,11 +1,24 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using PengajuanCutiOnline.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace PengajuanCutiOnline.Context
 {
-    public class MyContext
+    public class MyContext : DbContext
     {
+        public MyContext(DbContextOptions<MyContext> dbContext) : base(dbContext)
+        {
+
+        }
+
+        // mengatur connection string (done)
+        // menambahkan model untuk diolah dan / atau migrasi
+
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<UserRole> UserRoles { get; set; }
+        public DbSet<Employee> Employees { get; set; }
     }
 }
